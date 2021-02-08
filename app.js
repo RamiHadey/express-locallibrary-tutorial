1045a842920db2ca4fb2c314bd8deffbd976661d
Rami Hadey <Rhadey216@west-mec.org>
2021-02-08 15:03:47 -0700

    setting up controller and testing routes

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var catalogRouter = require('./routes/catalog'); //import routes for "catalog" area of the site
 var app = express();
+
+//Set up mongoose connection
+// var mongoose = require('mongoose');
+// var mongoDB = 'mongodb+srv://admin:2122232426Rami@cluster0.kdalz.mongodb.net/local_library?retryWrites=true&w=majority'
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:2122232426Rami@cluster0.ttvqq.mongodb.net/Cluster0?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
